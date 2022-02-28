362c5f0da2e7e1b2a42b48a216cb25e83a5b8903
leezer94 <2kunhee94@gmail.com>
2022-03-01 01:48:35 +0900

    Feat: add nav templates

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
-// do something!
+import { $ } from './common/DOM.js';
+import { bindEventListeners, renderNewsPage } from './components/index.js';
+
+const initApp = () => {
+  const $root = $('#root');
+
+  window.addEventListener('DOMContentLoaded', renderNewsPage($root));
+  bindEventListeners($root);
+};
+
+initApp();
