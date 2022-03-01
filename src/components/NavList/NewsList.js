e051d7e2b876820b743eb64ca7aa954337d98a40
leezer94 <2kunhee94@gmail.com>
2022-03-01 21:59:11 +0900

    Refactor: convert hard coded value to constant

--- src/components/NavList/NewsList.js
+++ src/components/NavList/NewsList.js
@@ -1,24 +1,36 @@
-import { CATEGORIES, ALTERNATIVE } from '../../common/constants.js';
+import {
+  CATEGORIES,
+  ALTERNATIVE,
+  POSITION,
+  CLASSLIST,
+} from '../../common/constants.js';
 import { $ } from '../../common/DOM.js';
 import { render } from '../../common/utils.js';
 import { newsArticleTemplate } from './newsListTemplate.js';
 
-export const renderNewsSection = async (category) => {
-  const articles = [
-    ...(await getNewsData(category ? category : CATEGORIES.BUSINESS.EN)),
-  ];
+export const renderNewsSection = async (position, articles, category) => {
+  if (!articles) {
+    articles = [
+      ...(await getNewsData(category ? category : CATEGORIES.GENERAL.EN)),
+    ];
+  }
+
   const newsList = $('.news-list');
 
   newsList.innerHTML = '';
 
   articles.map((article) => {
-    renderArticles(newsList, article);
+    renderArticles(newsList, position, article);
   });
 };
 
-const renderArticles = (target, { url, urlToImage, title, description }) => {
+export const renderArticles = (
+  target,
+  position,
+  { url, urlToImage, title, description }
+) => {
   if (!urlToImage) {
-    urlToImage = ALTERNATIVE.IMG;
+    urlToImage = 'img/img-not-found.jpeg';
   }
 
   if (!description) {
@@ -26,24 +38,40 @@ const renderArticles = (target, { url, urlToImage, title, description }) => {
   }
   render(
     target,
-    'afterbegin',
+    position,
     newsArticleTemplate(url, urlToImage, title, description)
   );
 };
 
-export const getNewsData = async (category) => {
-  let index = 1;
+export const loadMoreNews = (categoryList, target, page) => {
+  categoryList.map(async (category) => {
+    if (category.classList.contains(CLASSLIST.ACTIVE)) {
+      category = category.id;
 
+      let articles = [...(await getNewsData(category, page))];
+
+      articles.map((article) => {
+        renderArticles(target, POSITION.BEFOREEND, article);
+      });
+    }
+  });
+};
+
+const getNewsData = async (category, page = 1) => {
+  let index = 5;
+
+  console.log(page);
   try {
     const response = await axios.get(
       `https://newsapi.org/v2/top-headlines?country=kr&category=${
-        category === 'all' ? 'general' : category
-      }&pageSize=${index}&apiKey=d4d4acef12374e57b8c24c1b0aa1d634`
+        category === 'all' ? CATEGORIES.GENERAL.EN : category
+      }&page=${page}&pageSize=${index}&apiKey=9b2228495d834fdb93c5109f9aa4a0e6`
     );
+
     const articles = response.data.articles;
 
     return articles;
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 };
